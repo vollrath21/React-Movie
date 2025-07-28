22234df2ced1f906de584b09f13e005632ace3cc
Cameron Vollrath <vollrath@mines.edu>
2025-07-28 10:20:32 -0600

    Reference Fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './css/App.css'
 import MovieCard from './components/MovieCard'
 import Home from './pages/Home'
 import { Routes, Route } from 'react-router-dom'
